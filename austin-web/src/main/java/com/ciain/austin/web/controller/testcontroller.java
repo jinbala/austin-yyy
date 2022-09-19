fce88168d93eab1512d16039fe99fbafacea7826
ciain <1623152676@qq.com>
2022-09-19 12:36:15 +0800

    引入日志框架

--- austin-web/src/main/java/com/ciain/austin/web/controller/testcontroller.java
+++ austin-web/src/main/java/com/ciain/austin/web/controller/testcontroller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class testcontroller {
     @RequestMapping("/test")
     public String test(){
+        System.out.println("woxinsuile66666 diaoyong 一次 打印一次");
         return "testdemo6666";
     }
 }
